ffe11c54a1bfde0b78d8e195150a2cc64d5ecfa0
Levin Kerschberger <levinkerschberger@novatec-gmbh.de>
2023-01-29 18:26:29 +0100

    Revert "Datenmodellierung"
    This reverts commit a979c4975e673e59d236dedd5f32cf3351d93580.

--- src/main/java/com/test/models/Comment.java
+++ src/main/java/com/test/models/Comment.java
@@ -1,25 +1,26 @@
 package com.test.models;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-import static javax.persistence.GenerationType.AUTO;
-
 @Entity
-@Getter
-@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Comment {
 
     @Id
-    @GeneratedValue(strategy = AUTO)
-    private Long id;
+    @GeneratedValue
+    private int Id;
 
     @NotBlank
     private String content;
